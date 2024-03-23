cdf34d86bd7bb253097c606f96c21cf655ab9ef2
SatakeYusuke19920527 <satkyusk@icloud.com>
2024-03-23 22:19:05 +0900

    commit

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/LoginForm/LoginForm";
-
-export default function AuthPage() {
-  return (
-    <div className="flex flex-col h-screen justify-center items-center">
-        <LoginForm />
-    </div>
-  );
-}
